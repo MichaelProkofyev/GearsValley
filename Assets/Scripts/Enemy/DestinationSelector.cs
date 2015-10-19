0a5fc8ef102eb072a40ff2f64f8eaa8c15e1d96a
Mike Prokofyev <mikeprokofyev@gmail.com>
2015-10-20 00:32:18 +0500

    So, end of the line for now. Idea doesn't work. Maybe later i'll return to it.

---
+++ Assets/Scripts/Enemy/DestinationSelector.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestinationSelector : MonoBehaviour {
+
+	public GameObject[] patrolPoints;
+//	public Vector3 nextDestination;
+
+	private int patrolPointIdx = 0;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public Vector3 NextPatrolPoint () {
+		if (patrolPoints.Length == 0) {
+			Debug.Log ("!No points to patrol!");
+			return Vector3.zero;
+		}
+
+		int currentPatrolPointIdx = patrolPointIdx;
+		patrolPointIdx = ++patrolPointIdx % patrolPoints.Length;
+		return patrolPoints[currentPatrolPointIdx].transform.position;
+	}	
+}
--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -4,16 +4,20 @@ using System.Collections;
 public class EnemyController : MonoBehaviour {
 	
 	private EnemySight enemySight;
-		
+	private DestinationSelector destinationSelector;
+	private NavMeshAgent navAgent;
+
 
 
 	void Awake () {
 		enemySight = GetComponent<EnemySight>();
+		destinationSelector = GetComponent<DestinationSelector>();
+		navAgent = GetComponent<NavMeshAgent>();
 	}
 
 	// Use this for initialization
 	void Start () {
-	
+		navAgent.destination = destinationSelector.NextPatrolPoint();  //Start patrolling
 	}
 	
 	// Update is called once per frame
@@ -21,5 +25,8 @@ public class EnemyController : MonoBehaviour {
 		if (enemySight.playerInSight) {
 			transform.LookAt(enemySight.personalLastSighting);	
 		}
+
+		if (navAgent.remainingDistance < 0.5f)
+			navAgent.destination = destinationSelector.NextPatrolPoint();
 	}
 }
